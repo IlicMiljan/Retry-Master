bb88b7511556f5604e7b989b96efa9615a7b5189
Miljan Ilic <hello@miljanilic.com>
2023-05-16 09:05:24 +0200

    Add Sleep Util Documentation

--- src/Util/Sleep.php
+++ src/Util/Sleep.php
@@ -2,8 +2,29 @@
 
 namespace IlicMiljan\RetryMaster\Util;
 
+/**
+ * Class Sleep
+ *
+ * The Sleep utility class is designed to provide a static method for delaying
+ * the execution of a script for a specified number of milliseconds.
+ *
+ * This class can be useful in a variety of contexts, such as implementing
+ * backoff policies in retry operations, simulating network latency in testing
+ * environments, or throttling requests to a third-party service.
+ *
+ * @package IlicMiljan\RetryMaster\Util
+ */
 class Sleep
 {
+    /**
+     * Halts the execution of the script for a specified number of milliseconds.
+     *
+     * This static method uses the time_nanosleep function to pause the script,
+     * which allows for more precise delay times than the sleep and usleep
+     * functions.
+     *
+     * @param int $milliseconds The number of milliseconds to sleep.
+     */
     public static function milliseconds(int $milliseconds): void
     {
         $seconds = floor($milliseconds / 1000);
