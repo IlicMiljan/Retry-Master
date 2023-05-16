cd405d0223dd6cb36159268bf9c6ff4af531399c
Miljan Ilic <hello@miljanilic.com>
2023-05-16 09:06:45 +0200

    Add Retry Template Execute With Recovery Option and  Documentation

--- src/RetryTemplate.php
+++ src/RetryTemplate.php
@@ -3,6 +3,7 @@
 namespace IlicMiljan\RetryMaster;
 
 use Exception;
+use IlicMiljan\RetryMaster\Callback\RecoveryCallback;
 use IlicMiljan\RetryMaster\Callback\RetryCallback;
 use IlicMiljan\RetryMaster\Context\RetryContext;
 use IlicMiljan\RetryMaster\Policy\Backoff\BackoffPolicy;
@@ -13,8 +14,26 @@ use IlicMiljan\RetryMaster\Statistics\InMemoryRetryStatistics;
 use IlicMiljan\RetryMaster\Statistics\RetryStatistics;
 use IlicMiljan\RetryMaster\Util\Sleep;
 
+/**
+ * Class RetryTemplate
+ *
+ * The RetryTemplate class is the central component of the RetryMaster library.
+ * It handles the execution of operations that may fail, applying a retry policy
+ * and a backoff policy to manage retries. It also collects statistics about
+ * retry attempts.
+ *
+ * This class is designed to be flexible and extensible. The retry and backoff
+ * policies can be customized by passing in implementations of the RetryPolicy
+ * and BackoffPolicy interfaces, respectively. By default, it uses a max
+ * attempts retry policy with a limit of 3 attempts and a fixed backoff policy.
+ *
+ * @package IlicMiljan\RetryMaster
+ */
 class RetryTemplate
 {
+    /**
+     * The default maximum number of retry attempts.
+     */
     public const DEFAULT_MAX_ATTEMPTS = 3;
 
     private RetryPolicy $retryPolicy;
@@ -32,8 +51,18 @@ class RetryTemplate
     }
 
     /**
-     * @throws Exception
-     * @return mixed
+     * Executes the given RetryCallback, handling retries according to the
+     * configured retry and backoff policies.
+     *
+     * This method will continue to retry the operation until the retry policy
+     * determines that a retry should not
+     * be attempted, at which point the last exception thrown by the operation
+     * will be rethrown.
+     *
+     * @param RetryCallback $retryCallback The operation to retry.
+     * @throws Exception If the operation fails and the retry policy determines
+     *                   that a retry should not be attempted.
+     * @return mixed The result of the operation.
      */
     public function execute(RetryCallback $retryCallback)
     {
@@ -66,6 +95,59 @@ class RetryTemplate
         }
     }
 
+    /**
+     * Executes the given RetryCallback, handling retries according to the
+     * configured retry and backoff policies. If all retry attempts fail,
+     * this method will execute the provided RecoveryCallback.
+     *
+     * This method will continue to retry the operation until the retry policy
+     * determines that a retry should not be attempted, at which point it will
+     * execute the RecoveryCallback and return its result instead of throwing
+     * an exception.
+     *
+     * @param RetryCallback $retryCallback The operation to retry.
+     * @param RecoveryCallback $recoveryCallback The recovery operation to
+     *                                           execute if all retries fail.
+     * @throws Exception If the recovery operation itself throws an exception.
+     * @return mixed The result of the operation or the result of the recovery
+     *               operation if all retries fail.
+     */
+    public function executeWithRecovery(RetryCallback $retryCallback, RecoveryCallback $recoveryCallback)
+    {
+        $context = new RetryContext();
+
+        while (true) {
+            $context->incrementRetryCount();
+            $this->retryStatistics->incrementTotalAttempts();
+
+            try {
+                $result = $retryCallback->doWithRetry($context);
+
+                $this->retryStatistics->incrementSuccessfulAttempts();
+
+                return $result;
+            } catch (Exception $e) {
+                $this->retryStatistics->incrementFailedAttempts();
+
+                if (!$this->retryPolicy->shouldRetry($e, $context)) {
+                    return $recoveryCallback->recover($context);
+                }
+
+                $context->setLastException($e);
+
+                $sleepTime = $this->backoffPolicy->backoff($context->getRetryCount());
+                $this->retryStatistics->incrementSleepTime($sleepTime);
+
+                Sleep::milliseconds($sleepTime);
+            }
+        }
+    }
+
+    /**
+     * Returns the RetryStatistics instance being used by this RetryTemplate.
+     *
+     * @return RetryStatistics The current RetryStatistics instance.
+     */
     public function getRetryStatistics(): RetryStatistics
     {
         return $this->retryStatistics;
