d4f139c30f344d870a999ceb57ca88ee4c30e7c4
Miljan Ilic <hello@miljanilic.com>
2023-05-16 08:50:05 +0200

    Add Callbacks Documentation

--- src/Callback/RecoveryCallback.php
+++ src/Callback/RecoveryCallback.php
@@ -4,11 +4,34 @@ namespace IlicMiljan\RetryMaster\Callback;
 
 use IlicMiljan\RetryMaster\Context\RetryContext;
 
+/**
+ * Interface RecoveryCallback
+ *
+ * RecoveryCallback is an interface to be implemented by classes that define a
+ * recovery operation to be executed when all attempts of a retry operation have
+ * failed.
+ *
+ * The recovery operation is defined in the recover method, which takes a
+ * RetryContext parameter and returns a result. This context provides
+ * information about the failed retry operation, such as the number of attempts
+ * and the last exception that caused the operation to fail.
+ *
+ * The return type of the recover method is indicated by the return type hint of
+ * 'mixed', allowing the RecoveryCallback to be used for a wide variety of
+ * recovery operations.
+ *
+ * @package IlicMiljan\RetryMaster\Callback
+ */
 interface RecoveryCallback
 {
     /**
-     * @param RetryContext $context
-     * @return mixed
+     * Executes the recovery operation when all attempts of a retry operation
+     * have failed.
+     *
+     * @param RetryContext $context The context of the failed retry operation,
+     *                              containing information such as the number of
+     *                              attempts and the last exception.
+     * @return mixed The result of the recovery operation.
      */
     public function recover(RetryContext $context);
 }
--- src/Callback/RetryCallback.php
+++ src/Callback/RetryCallback.php
@@ -4,11 +4,32 @@ namespace IlicMiljan\RetryMaster\Callback;
 
 use IlicMiljan\RetryMaster\Context\RetryContext;
 
+/**
+ * Interface RetryCallback
+ *
+ * RetryCallback is an interface that should be implemented by classes that
+ * define an operation to be retried. The operation is defined in the
+ * doWithRetry method, which accepts a RetryContext parameter and returns a
+ * result.
+ *
+ * The RetryContext provides information about the retry operation, such as the
+ * number of attempts so far and the last exception that caused the operation
+ * to fail.
+ *
+ * The result of the doWithRetry method can be any type (as indicated by the
+ * return type hint of 'mixed'). This allows the RetryCallback to be used for a
+ * wide variety of operations.
+ *
+ * @package IlicMiljan\RetryMaster\Callback
+ */
 interface RetryCallback
 {
     /**
-     * @param RetryContext $context
-     * @return mixed
+     * Executes the operation to be retried.
+     *
+     * @param RetryContext $context The context of the retry operation, containing
+     * information such as the number of attempts so far and the last exception.
+     * @return mixed The result of the operation.
      */
     public function doWithRetry(RetryContext $context);
 }
